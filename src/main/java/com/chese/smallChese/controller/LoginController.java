2bc18e521b79e14a350b304e1b560731828c2571
mmall <1249444369@qq.com>
2019-05-18 01:55:16 +0800

    新增article对象以及接口

--- src/main/java/com/chese/smallChese/controller/LoginController.java
+++ src/main/java/com/chese/smallChese/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.chese.smallChese.service.IUserService;
+import com.chese.smallChese.utils.FormatTimeUtil;
 import com.chese.smallChese.utils.HttpClientUtil;
 import com.chese.smallChese.utils.MD5Util;
 
@@ -19,9 +20,12 @@ import net.sf.json.JSONObject;
 public class LoginController {	
 	
 	private String url = "https://api.weixin.qq.com/sns/jscode2session?";
-	private String appid = "wxe414ac7f64ebfd82";
-	private String secret = "e4392af08a5e6b8e959c28dd1c7cf36e";
-	private String grant_type = "authorization_code";
+	private String grant_type = "authorization_code";           
+	//private String appid = "wxe414ac7f64ebfd82";				  ---个人appid
+	//private String secret = "e4392af08a5e6b8e959c28dd1c7cf36e"; ---个人秘钥
+	
+	private String appid = "wx98029e1fb23c1dbe";  				//企业appid
+	private String secret = "5f49d9b7bbacff0f214119a88491a21b"; //企业秘钥
 	
 	@Autowired
 	private HttpClientUtil httpClient;
@@ -43,9 +47,9 @@ public class LoginController {
 	
 	@RequestMapping("getSessionKey")
 	@ResponseBody
-	public JSONObject login(String code){
+	public JSONObject login(String code,String nickName,Integer gender,String province,String city,String country){
 		System.out.println("===login被调用===");
-		if(code == null){
+		if(code == null || code == ""){
 			HashMap<String,Object> resultMap = new HashMap<>();
 			resultMap.put("errcode", -2);
 			resultMap.put("errmsg", "code不能为空");
@@ -70,6 +74,12 @@ public class LoginController {
 					HashMap<String,Object> paramMap = new HashMap<String,Object>();
 					paramMap.put("openId", openId);
 					paramMap.put("sessionKey", sessionKey);
+					paramMap.put("nickName", nickName);
+					paramMap.put("gender", gender);
+					paramMap.put("province", province);
+					paramMap.put("city", city);
+					paramMap.put("country", country);
+					paramMap.put("creatTime", FormatTimeUtil.getTimestamp());
 					userService.addUser(paramMap);
 					
 					HashMap<String, Object> resultMap = new HashMap<>();
@@ -78,6 +88,7 @@ public class LoginController {
 					resultMap.put("openId", openId);
 					resultMap.put("errcode", 0);
 					JSONObject jsonObject = JSONObject.fromObject(resultMap);
+					
 					return jsonObject;
 				}else{
 					//如果userResult不为空，说明用户已经登录过，现在是更新session_key
@@ -92,6 +103,7 @@ public class LoginController {
 					resultMap.put("openId", openId);
 					resultMap.put("errcode", 0);
 					JSONObject jsonObject = JSONObject.fromObject(resultMap);
+					
 					return jsonObject;
 				}
 				
